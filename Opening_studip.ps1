bad070365d2bf78c8dc9485191749ed372cfa44d
rawleyperetz <70651978+rawleyperetz@users.noreply.github.com>
2022-03-05 14:10:12 +0100

    Update Opening_studip.ps1

--- Opening_studip.ps1
+++ Opening_studip.ps1
@@ -1,7 +1,10 @@
-﻿$internet_test = ping google.com
+ #Powershell script for studip
+ $internet_test = ping google.com
 
+#If your internet is fast, skip the next immediate code
 Start-Sleep -Seconds 5
 
+#If-condition to check whether you are connected to the internet
 if ($internet_test.EndsWith("again.") -eq 'True') {
 
  [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
@@ -15,6 +18,7 @@ $form.Size = New-Object System.Drawing.Size(330,270)
 $form.MaximumSize = $form.Size 
 $form.Text = "StudIP"
 
+#Username label
 $first_text = New-Object System.Windows.Forms.Label
 $first_text.Size = New-Object System.Drawing.Size(90,20)
 $first_text.Text = "Username: "
@@ -22,6 +26,7 @@ $first_text.Top = 20
 $first_text.Left = 20
 #$first_text.Anchor = 'left,top'
 
+#Username Entry Field
 $user = New-Object System.Windows.Forms.TextBox
 $user.Size = New-Object System.Drawing.Size(250,50) 
 $user.Top  = 50
@@ -30,13 +35,14 @@ $user.Left = 20
 $user.Width = 250
 #$user.Anchor = 'right,top'
 
+#Password label
 $sec_text = New-Object System.Windows.Forms.Label
 $sec_text.Size = New-Object System.Drawing.Size(200,20) 
 $sec_text.Text = "Password: "
 $sec_text.Top = 80
 $sec_text.Left = 20
 
-
+#Password Field
 $password = New-Object System.Windows.Forms.TextBox
 #$password.Size = New-Object System.Drawing.Size(250,50)
 $password.UseSystemPasswordChar = "*" 
@@ -45,6 +51,7 @@ $password.Left = 20
 $password.Height = 50
 $password.Width = 250
 
+#Ok button click function
 $ok_click = {
  $user_val = $user.Text 
  $pass_val = $password.Text 
@@ -52,6 +59,8 @@ $ok_click = {
  python .\studips.py $user_val $pass_val
  #$form.Close()
  }
+ 
+#Ok Button
 $ok_button = New-Object System.Windows.Forms.Button
 $ok_button.Size = New-Object System.Drawing.Size(100,50)
 #$ok_button.Location = New-Object System.Drawing.Size(140,44)
@@ -68,13 +77,6 @@ $form.Controls.Add($sec_text)
 $form.Controls.Add($user)
 $form.Controls.Add($password)
 $form.Controls.Add($ok_button)
-
-#$secstr = $password.Text | ConvertTo-SecureString -AsPlaintText -Force
- #[System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null
- #Add-Type -AssemblyName Microsoft.VisualBasic
- #$pass_val = [Microsoft.VisualBasic.Interaction]::InputBox("Enter the studip password","Password prompt")
- #$pass_val = $password.Text
- 
- $form.ShowDialog()
+$form.ShowDialog()
 }
 
